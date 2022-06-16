73fa635fb6befbf5e768632e7df7d0a0fdefdee0
Bruna Valnei <bruna.valnei@gmail.com>
2022-06-16 00:28:34 -0300

    steps novos

--- src/main/java/stepDefinitions/LoginSteps.java
+++ src/main/java/stepDefinitions/LoginSteps.java
@@ -2,25 +2,42 @@ package stepDefinitions;
 
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import io.restassured.RestAssured;
+import io.restassured.internal.common.assertion.Assertion;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
 
 public class LoginSteps {
 
-    @Given("^I have login and password$")
-    public void iHaveLoginAndPassword() throws Throwable {
-        String url = "https://api-de-tarefas.herokuapp.com";
+    private static Response response;
+
+
+    @Given("^I have url \"([^\"]*)\"$")
+    public void iHaveUrl(String url) throws Throwable {
         RestAssured.baseURI = url;
-        RequestSpecification httpRequest = RestAssured.given();
-        Response response = given()
+    }
+
+//    @Given("^I have route \"([^\"]*)\"$")
+//    public void iHaveRoute(String basePath) throws Throwable {
+//        response = request.basePath(basePath);
+//        get
+//        //        RestAssured.basePath = basePath;
+//    }
+
+    @Given("^I use the body with email \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void iUseTheBody(String email, String password) throws Throwable {
+        response = given()
                 .relaxedHTTPSValidation()
                 .auth()
                 .preemptive()
-                .basic("batata@gmail.com", "123456")
+                .basic(email, password)
                 .body("{\n" +
                         "  \"session\": {\n" +
                         "        \"email\": \"batata@gmail.com\",\n" +
@@ -29,6 +46,45 @@ public class LoginSteps {
                         "}")
                 .accept("application/vnd.api+json")
                 .contentType("application/json")
+                .when()
+                .post("/sessions")
+                .then().extract().response();
+    }
+
+
+//    @Given("^I send the POST request$")
+//    public void iSendThePostRequest() throws Throwable {
+//        response = (RequestSpecification) request.post("/sessions");
+//    }
+
+    @Then("^Http response should be (\\d+)$")
+    public void httpResponseShouldBe(int statusCode) throws Throwable {
+        Assert.assertEquals(statusCode, response.getStatusCode());
+    }
+
+    @Then("^Save auth token$")
+    public void save_auth_token() throws Throwable {
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        String authToken = jsonPathEvaluator.get("data.attributes.auth-token");
+        System.out.println(authToken);
+    }
+
+    @Test
+    @Given("^I have login and password$")
+    public void iHaveLoginAndPassword() throws Throwable {
+        String url = "https://api-de-tarefas.herokuapp.com";
+        RestAssured.baseURI = url;
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = given()
+                .relaxedHTTPSValidation()
+                .accept("application/vnd.api+json")
+                .contentType("application/json")
+                .body("{\n" +
+                        "  \"session\": {\n" +
+                        "        \"email\": \"batata@gmail.com\",\n" +
+                        "        \"password\": \"123456\"\n" +
+                        "  }\n" +
+                        "}")
                 .when()
                 .post("/sessions")
                 .then()
--- src/test/java/feature/login.feature
+++ src/test/java/feature/login.feature
@@ -1,4 +1,10 @@
 Feature: POST login
 
+  @Positive
   Scenario: Login
-    Given I have login and password
+    Given I have url "https://api-de-tarefas.herokuapp.com"
+#    * I have route "/sessions"
+    * I use the body with email "batata@gmail.com" and password "123456"
+#    * I send the POST request
+    Then Http response should be 200
+    * Save auth token
