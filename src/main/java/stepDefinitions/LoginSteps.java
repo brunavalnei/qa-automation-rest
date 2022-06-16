0651d0148e29a1b60585d06a7c825598854dbc65
Bruna Valnei <bruna.valnei@gmail.com>
2022-06-16 20:55:46 -0300

    criação de steps reutilizáveis

--- src/main/java/stepDefinitions/LoginSteps.java
+++ src/main/java/stepDefinitions/LoginSteps.java
@@ -7,57 +7,60 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import model.CustomerPojo;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
 public class LoginSteps {
 
+    CustomerPojo customer = new CustomerPojo();
     private static Response response;
+    private RequestSpecification request;
 
-
-    @Given("^I have url \"([^\"]*)\"$")
+    @Given("^I have baseURI \"([^\"]*)\"$")
     public void iHaveUrl(String url) throws Throwable {
         RestAssured.baseURI = url;
     }
 
-//    @Given("^I have route \"([^\"]*)\"$")
-//    public void iHaveRoute(String basePath) throws Throwable {
-//        response = request.basePath(basePath);
-//        get
-//        //        RestAssured.basePath = basePath;
-//    }
+    @Given("^I use header$")
+    public void iUseHeader() throws Throwable {
+        request = given()
+                .header("accept", "application/vnd.api+json")
+                .header("Content-type", "application/json")
+                .log()
+                .headers();
+    }
 
     @Given("^I use the body with email \"([^\"]*)\" and password \"([^\"]*)\"$")
     public void iUseTheBody(String email, String password) throws Throwable {
-        Map<String, Object> body = new HashMap<String, Object>();
-        body.put("email", email);
-        body.put("password", password);
-
-        response = given()
-                .body(body)
-                .accept("application/vnd.api+json")
-                .contentType("application/json")
-                .when()
-                .post("/sessions")
-                .then().extract().response();
+        customer.setEmailUser(email);
+        customer.setPassword(password);
     }
 
-//    @Given("^I send the POST request$")
-//    public void iSendThePostRequest() throws Throwable {
-//        response = (RequestSpecification) request.post("/sessions");
-//    }
+    @Given("^I send the POST request with endpoint \"([^\"]*)\"$")
+    public void iSendThePostRequest(String endpoint) throws Throwable {
+        response = request
+                .when()
+                .body("{\n" +
+                        "  \"session\": {\n" +
+                        "        \"email\":\"" + customer.getEmailUser() + "\", " + "\n"+
+                        "        \"password\": \"" + customer.getPassword()  + "\" " + "\n"+
+                        "  }\n" +
+                        "}")
+                .log()
+                .body()
+                .post(endpoint)
+                .then().extract().response();
+    }
 
     @Then("^Http response should be (\\d+)$")
     public void httpResponseShouldBe(int statusCode) throws Throwable {
         Assert.assertEquals(statusCode, response.getStatusCode());
     }
 
-    @Then("^Save auth token$")
+    @Then("^I save the auth token$")
     public void save_auth_token() throws Throwable {
         JsonPath jsonPathEvaluator = response.jsonPath();
         String authToken = jsonPathEvaluator.get("data.attributes.auth-token");
--- src/test/java/feature/login.feature
+++ src/test/java/feature/login.feature
@@ -2,9 +2,9 @@ Feature: POST login
 
   @Positive
   Scenario: Login
-    Given I have url "https://api-de-tarefas.herokuapp.com"
-#    * I have route "/sessions"
+    Given I have baseURI "https://api-de-tarefas.herokuapp.com"
+    * I use header
     * I use the body with email "batata@gmail.com" and password "123456"
-#    * I send the POST request
+    * I send the POST request with endpoint "/sessions"
     Then Http response should be 200
-    * Save auth token
+    * I save the auth token
